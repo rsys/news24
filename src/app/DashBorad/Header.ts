ff666b0d2013d8b27fe52bfdaaec7c65b2b96ad0
ravigupta924@gmail.com <ravigupta924@gmail.com>
2018-03-31 18:02:28 +0530

    lll

---
+++ src/app/DashBorad/Header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-header',
+  templateUrl: './Header.html',
+  
+})
+export class HeaderComponent {
+  title = 'footer';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { ModuleWithProviders,NgModule } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { footerComponent } from './DashBorad/footer';
+import { HeaderComponent } from './DashBorad/Header';
+import { DashBoardComponent } from './DashBorad/dashboard';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: DashBoardComponent },
+ 
+];
+//const rootRouting: ModuleWithProiders = RouterModule.forRoot([]);
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    footerComponent,
+    HeaderComponent,
+    DashBoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
